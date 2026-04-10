bcece5791a3af1cc42be112585a91015fab88a4c
Nathan102939 <nathan.oliver@dcmail.ca>
2026-04-10 19:47:32 -0400

    New fix
    Added the Darken and the CSS for it to even make it work

--- lab4/part2/someodd.js
+++ lab4/part2/someodd.js
@@ -33,3 +33,15 @@ function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.classList.remove("dark");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  } else {
+    btn.classList.add("dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+});
