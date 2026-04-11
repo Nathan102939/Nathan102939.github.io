020d6d455fe747c98227770e24f51e8001176f50
Nathan102939 <nathan.oliver@dcmail.ca>
2026-04-10 20:30:39 -0400

    Update someodd.js
    Shape base class and Ball class with movement

--- lab4/part3/someodd.js
+++ lab4/part3/someodd.js
@@ -14,3 +14,39 @@ function random(min,max) {
 function randomRGB() {
   return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
 }
+
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.velX = -this.velX;
+    }
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.velY = -this.velY;
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
