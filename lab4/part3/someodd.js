46d469d3060cac07a7cf3d38bf1802053f7cdf54
Nathan102939 <nathan.oliver@dcmail.ca>
2026-04-10 20:21:03 -0400

    Update someodd.js
    setup canvas, random functions, and base variables

---
+++ lab4/part3/someodd.js
@@ -0,0 +1,16 @@
+const para = document.querySelector('p');
+let count = 0;
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+function random(min,max) {
+  return Math.floor(Math.random()*(max-min)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
+}
