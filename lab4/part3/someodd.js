e6d9e498de151b5cda6cbe8a0a7e6a4595f8521a
Nathan102939 <nathan.oliver@dcmail.ca>
2026-04-10 20:38:33 -0400

    Update someodd.js
    added ball collision detection and ball generation system

--- lab4/part3/someodd.js
+++ lab4/part3/someodd.js
@@ -50,3 +50,34 @@ class Ball extends Shape {
     this.y += this.velY;
   }
 }
+
+Ball.prototype.collisionDetect = function() {
+  for (const ball of balls) {
+    if (!(this === ball) && ball.exists) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB();
+      }
+    }
+  }
+};
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
+}
